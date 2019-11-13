259a460c9edd5191cf3012a6a66682d219b9834d
Mikkel Wexøe Ertbjerg <44799207+mikkelertbjerg@users.noreply.github.com>
2019-11-13 22:01:44 +0100

    Uploaded the mom project.

---
+++ EMI/MOM-Create-PDF/pdfGenerator MoM/PdfCreator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using IronPdf;
+
+namespace pdfGenerator_MoM
+{
+    public class PdfCreator
+    {
+        public string OutPutDir { get; set; }
+
+        public PdfCreator()
+        {
+            OutPutDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+        /// <summary>
+        /// Takes a raw string from a message, manipulates it and prints out a pdf to the OutPutDir, if none is chosen it defaults to desktop
+        /// </summary>
+        /// <param name="rawInput">This is the raw string from the message, do not edit it</param>
+        public void Create(string rawInput)
+        {
+            string[] splitString = rawInput.Split(' ');
+
+
+            // good luck reading this.
+            var html = GenerateHtml(splitString[0], splitString[1], splitString[2], splitString[3],
+                splitString[4], splitString[5], splitString[6], splitString[7],
+                splitString[8], splitString[9], splitString[10], splitString[11], splitString[12], splitString[13]);
+
+            IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
+            Renderer.PrintOptions.CreatePdfFormsFromHtml = true;
+            Renderer.RenderHtmlAsPdf(html).SaveAs(OutPutDir);
+        }
+
+        private string GenerateHtml(string name, string lastName,  string license, string gender, string color, 
+            string brand, string model, string doors, string fuel, string type, string price, string animals, 
+            string pickupLocation, string dropOffLocation)
+        {
+            var formHtml = $@"<html>
+                                 <body>
+                                     <h1 align='center'>Faraday Contract</h1>
+                                     <h2> Name </h2> <p>{name} { lastName} License nr: {license}, {gender}</p>
+
+                                     <h2> car </h2> <p>{brand} {model} {type} { color} {doors} doors, allowed animals: {animals} fuel: {fuel} price: {price}</ p >
+                                     <h2> Location </h2> <p> is to be picked up at {pickupLocation} and dropped off at {dropOffLocation}  </p>
+
+                                </ body >
+                            </html>";
+
+            return formHtml;
+        }
+    }
+}
---
+++ EMI/MOM-Create-PDF/pdfGenerator MoM/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace pdfGenerator_MoM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfCreator pdfCreater = new PdfCreator();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "FinishedOrders", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                    exchange: "FinishedOrders",
+                    routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for orders.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    pdfCreater.Create(message);
+
+                    Console.WriteLine(" Great Success File printed");
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}

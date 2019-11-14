e6b2d522a22283dfa7f5320860edef312cd4c824
mikkelertbjerg <mikkelertbjerg@gmail.com>
2019-11-14 08:40:14 +0100

    Hidden login info

--- EMI/Faraday-BE-gRPC/Database/DatabaseConnection.cs
+++ EMI/Faraday-BE-gRPC/Database/DatabaseConnection.cs
@@ -21,20 +21,20 @@ namespace Faraday_BE_gRPC.Database
 
         private void Initialize()
         {
-            server = "mysql50.unoeuro.com";
-            database = "mikkelertbjerg_dk_db_ls";
-            user = "mikkelertbjerg_dk";
-            password = "knockers2019";
+            server = "server"; //Fields are given dummy values.
+            database = "database"; //Fields are given dummy values.
+            user = "user"; //Fields are given dummy values.
+            password = "password"; //Fields are given dummy values.
             string connectionString;
             connectionString = $"Server={server};Database={database};Uid={user};Pwd={password}";
         }
 
         public static string GetConnectionString()
         {
-            string server = "mysql50.unoeuro.com";
-            string database = "mikkelertbjerg_dk_db_ls";
-            string user = "mikkelertbjerg_dk";
-            string password = "knockers2019";
+            string server = "server"; //Fields are given dummy values.
+            string database = "database"; //Fields are given dummy values.
+            string user = "user"; //Fields are given dummy values.
+            string password = "password"; //Fields are given dummy values.
             string connectionString;
             return connectionString = "Server=" + server + ";" + "Database=" + database + ";" +
                 "Uid=" + user + ";" + "Pwd=" + password + ";";
